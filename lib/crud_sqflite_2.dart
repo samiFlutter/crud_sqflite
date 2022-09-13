71ce083f8ff22f21773b476a8c6054104b3bb290
sami <sami.workmail.flutter@gmail.com>
2022-09-13 18:15:34 +0100

    after put each one in dart file

--- lib/crud_sqflite_2.dart
+++ lib/crud_sqflite_2.dart
@@ -4,6 +4,9 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 
+import 'grocery_model.dart';
+import 'my_databasehelper.dart';
+
 class SqliteApp extends StatefulWidget {
   const SqliteApp({Key? key}) : super(key: key);
 
@@ -89,81 +92,5 @@ class _SqliteAppState extends State<SqliteApp> {
   }
 }
 
-class Grocery {
-  final int? id;
-  final String name;
-  Grocery({this.id, required this.name});
 
-  factory Grocery.fromMap(Map<String, dynamic> json) => new Grocery(
-        id: json['id'],
-        name: json['name'],
-      );
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-    };
-  }
-}
-
-class DatabaseHelper {
-  DatabaseHelper._privateConstructor();
-  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
-
-  static Database? _database;
-  Future<Database> get database async => _database ??= await _initDatabase();
-
-  Future<Database> _initDatabase() async {
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, 'groceries.db');
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: _onCreate,
-    );
-  }
-
-  Future _onCreate(Database db, int version) async {
-    await db.execute('''
-    CREATE TABLE groceries(
-      id INTEGER PRIMARY KEY,
-      name TEXT
-    )
-''');
-  }
-
-  Future<List<Grocery>> getGroceries() async {
-    Database db = await instance.database;
-    var groceries = await db.query(
-      'groceries',
-      orderBy: 'name',
-    );
-
-    List<Grocery> groceriesList = groceries.isNotEmpty
-        ? groceries.map((c) => Grocery.fromMap(c)).toList()
-        : [];
-
-    return groceriesList;
-  }
-
-  Future<int> add(Grocery grocery) async {
-    Database db = await instance.database;
-    return await db.insert('groceries', grocery.toMap());
-  }
-
-  Future<int> remove(int id) async {
-    Database db = await instance.database;
-    return await db.delete('groceries', where: 'id = ?', whereArgs: [id]);
-  }
-
-  Future<int> update(Grocery grocery) async {
-    Database db = await instance.database;
-    return await db
-        .rawUpdate('UPDATE groceries SET name = ? WHERE id = ${grocery.id}', [
-      '${grocery.name}',
-    ]);
-    // return await db.update('groceries', grocery.toMap(),
-    //     where: 'id : ?', whereArgs: [grocery.id]);
-  }
-}
---
+++ lib/my_databasehelper.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+import 'grocery_model.dart';
+
+
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'groceries.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE groceries(
+      id INTEGER PRIMARY KEY,
+      name TEXT
+    )
+''');
+  }
+
+  Future<List<Grocery>> getGroceries() async {
+    Database db = await instance.database;
+    var groceries = await db.query(
+      'groceries',
+      orderBy: 'name',
+    );
+
+    List<Grocery> groceriesList = groceries.isNotEmpty
+        ? groceries.map((c) => Grocery.fromMap(c)).toList()
+        : [];
+
+    return groceriesList;
+  }
+
+  Future<int> add(Grocery grocery) async {
+    Database db = await instance.database;
+    return await db.insert('groceries', grocery.toMap());
+  }
+
+  Future<int> remove(int id) async {
+    Database db = await instance.database;
+    return await db.delete('groceries', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> update(Grocery grocery) async {
+    Database db = await instance.database;
+    return await db
+        .rawUpdate('UPDATE groceries SET name = ? WHERE id = ${grocery.id}', [
+      '${grocery.name}',
+    ]);
+    // return await db.update('groceries', grocery.toMap(),
+    //     where: 'id : ?', whereArgs: [grocery.id]);
+  }
+}
